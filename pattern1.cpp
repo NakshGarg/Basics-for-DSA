1b512578429978936ac3904b321beede50e1928b
Naksh Garg <nakshgarg2006@gmail.com>
2025-02-22 18:38:29 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,18 @@
+//wap to print the pattern of solid rectangle using stars
+#include<iostream>
+using namespace std;
+int main(){
+    int m,n;
+    cout<<"Enter how many rows and columns you want :- ";
+    cin>>m>>n;
+    for (int i=1; i<=m; i++)
+    {
+        for (int j=1; j<=n; j++)
+        { 
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
