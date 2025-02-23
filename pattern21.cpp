5ec48924e7d4657c0c563f10565744d469b8704a
Naksh Garg <nakshgarg2006@gmail.com>
2025-02-23 12:33:56 +0530

    Add files via upload

---
+++ pattern21.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j, n;
+    cout << "Rows? :- ";
+    cin >> n;
+
+    // Upper half
+    for (i = n; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        for (j = 1; j <= 2 * (n - i); j++) { 
+            cout << "  ";
+        }
+        for (j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    // Lower half
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        for (j = 1; j <= 2 * (n - i); j++) { 
+            cout << "  ";
+        }
+        for (j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
