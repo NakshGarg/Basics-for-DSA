612d89531bc6ac1ebfd20dd014a75ccee89ff42a
Naksh Garg <nakshgarg2006@gmail.com>
2025-02-23 14:26:11 +0530

    Update pattern2.cpp

--- pattern2.cpp
+++ pattern2.cpp
@@ -16,3 +16,12 @@ int main(){
     return 0;
 
 }
+
+// Enter how many rows and columns you want :- 7
+// 1 1 1 1 1 1 1 
+// 2 2 2 2 2 2 2 
+// 3 3 3 3 3 3 3 
+// 4 4 4 4 4 4 4 
+// 5 5 5 5 5 5 5 
+// 6 6 6 6 6 6 6 
+// 7 7 7 7 7 7 7 
